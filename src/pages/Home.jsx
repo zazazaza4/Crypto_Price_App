739071e9cfa8d0ccd802836e0e09e202f39aa70a
Yaroslav <6715414@gmail.com>
2023-05-16 22:34:17 +0300

    added Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useHomeStore } from "../stores";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const store = useHomeStore();
+
+  useEffect(() => {
+    store.fetchCoins();
+  }, []);
+
+  return (
+    <div>
+      <input type="text" value={store.query} onChange={store.setQuery} />
+      {store.coins.map((coin) => {
+        return (
+          <div key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
